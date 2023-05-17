75b5cfe4a4cf02b05dedb29303aeffce132fffff
Yensan2B <90729827+Yensan2B@users.noreply.github.com>
2023-05-17 14:14:35 -0400

    did more on the about page
    ye

--- FruitProject/fruitProject-frontend/src/pages/About.js
+++ FruitProject/fruitProject-frontend/src/pages/About.js
@@ -11,6 +11,7 @@ function About() {
   return (
     <div className="about_body">
       <h1 className="Title"> About us </h1>
+
       <div className="about_container">
         <div className="about_firstSection">
           <h1>
@@ -26,6 +27,24 @@ function About() {
         <div className="about_firstSection">
         <img src="https://d.ibtimes.co.uk/en/full/337219/eat-yourself-happier.jpg" alt="Girl in a jacket" width="500" height="600"/>
 
+        </div>
+      </div>
+
+      <div className="about_container">
+        <div className="about_firstSection">
+        <img className="img2"width="500" height="600"/>
+        </div>  
+        <div className="about_firstSection">
+        <h1>
+          <br/>
+            Goal</h1>
+          <p>
+          At the Objectively Best Fruit Tier List, 
+          we believe that fruits deserve the spotlight they often miss in the culinary world.
+           With so many varieties and flavors to choose from, we aim to be your go-to resource 
+           for all things fruity. Whether you're a seasoned fruit connoisseur or just starting your 
+           fruit exploration journey, we have something for everyone.
+          </p>
         </div>
       </div>
     </div>
